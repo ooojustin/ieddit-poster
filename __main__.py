5c25fb9ca4689dca16f1811a3bc5786ae4f62dd2
Justin <justin@garofolo.net>
2019-10-22 02:48:39 -0400

    updated settings template, implemented praw in main.py

--- __main__.py
+++ __main__.py
@@ -1,5 +1,18 @@
-import settings
-from ieddit_api import Client
+from settings import IEDDIT, REDDIT
+import settings, praw, ieddit
 
-client = Client(settings.USERNAME, settings.PASSWORD, settings._2CAPTCHA_API_KEY)
-client.login()
+reddit = praw.Reddit(
+    client_id = REDDIT["CLIENT_ID"],
+    client_secret = REDDIT["CLIENT_SECRET"],
+    user_agent = REDDIT["USER_AGENT"],
+    username = REDDIT["USERNAME"],
+    password = REDDIT["PASSWORD"]
+)
+
+
+ieddit = ieddit.Client(
+    username = IEDDIT["USERNAME"],
+    password = IEDDIT["PASSWORD"], 
+    _2captcha_api_key = settings._2CAPTCHA_API_KEY
+)
+
