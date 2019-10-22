2f86dd24cbe635f1a5ecc7abadeef6766d61ef95
Justin <justin@garofolo.net>
2019-10-22 04:10:39 -0400

    general functionality of the program completed :)

--- database.py
+++ database.py
@@ -8,8 +8,28 @@ conn = sqlite3.connect(db_path)
 conn.isolation_level = None # autocommit
 cursor = conn.cursor()
 
+def _create_insert_query(table, columns):
+    structure = "INSERT INTO {} ({}) VALUES ({})"
+    column_names = ", ".join(columns)
+    value_slots = ("?, " * len(columns))[:-2]
+    return structure.format(table, column_names, value_slots)
+
+def add_post(data):
+    columns = ["title", "ieddit_id", "reddit_id", "ieddit_url", "reddit_url", "subieddit", "subreddit", "image_url", "timestamp"]
+    query = _create_insert_query("posts", columns)
+    assert len(data) == len(columns), "number of columns and number of provided values does not match up."
+    cursor.execute(query, data)
+
+def submission_reposted(subieddit, reddit_id):
+    query = "SELECT * FROM posts WHERE subieddit = ? AND reddit_id = ?"
+    params = (subieddit, reddit_id)
+    cursor.execute(query, params)
+    return cursor.fetchone() is not None
+
 def table_exists(name):
-    cursor.execute("SELECT count(name) FROM sqlite_master WHERE type = 'table' AND name = ?", (name,))
+    query = "SELECT count(name) FROM sqlite_master WHERE type = 'table' AND name = ?"
+    params = (name,)
+    cursor.execute(query, params)
     return cursor.fetchone()[0] > 0
 
 def init():
@@ -18,12 +38,13 @@ def init():
         cursor.execute("""
             CREATE TABLE "posts" (
                 "id"	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                "title"         TEXT,
                 "ieddit_id"	INTEGER UNIQUE,
                 "reddit_id"	TEXT UNIQUE,
                 "ieddit_url"	TEXT,
                 "reddit_url"	TEXT,
-                "ieddit_sub"	TEXT,
-                "reddit_sub"	TEXT,
+                "subieddit"	    TEXT,
+                "subreddit"	    TEXT,
                 "image_url"     TEXT,
                 "timestamp"	INTEGER
             );""")
