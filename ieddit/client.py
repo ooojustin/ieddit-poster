e746413d4734a271e73471292ac7e3eccde55e97
Justin <justin@garofolo.net>
2019-10-23 14:22:04 -0400

    basic implementation of logging

--- ieddit/client.py
+++ ieddit/client.py
@@ -1,12 +1,17 @@
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, quote_plus
 from ._2captcha import _2Captcha
-import re, requests
+from datetime import datetime
+import re, requests, sys, os
 
 class Client:
 
     USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:71.0) Gecko/20100101 Firefox/71.0"
     IEDDIT = lambda e: urljoin("https://ieddit.com/", e)
+
+    LOG_DIR = os.path.join(os.path.dirname(os.path.abspath(sys.modules['__main__'].__file__)), "__logs__")
+    if not os.path.exists(LOG_DIR):
+        os.mkdir(LOG_DIR)
 
     def __init__(self, username, password, _2captcha_api_key, do_login = True):
 
@@ -71,16 +76,20 @@ class Client:
         }
 
         response = self.session.post(Client.IEDDIT("/create_post"), params)
-        post_url = response.url
-
-        pattern = r"\/i\/{}\/(\d+)\/".format(sub)
-        match = re.search(pattern, post_url)
 
         # retry if the captcha failed
         if "invalid captcha" in response.text:
             return self.create_post(title, sub, url, text, nsfw)
 
+        post_url = response.url
+        pattern = r"\/i\/{}\/(\d+)\/".format(sub)
+        match = re.search(pattern, post_url)
+
         if not match:
+            log_file_name = "create_post " + datetime.now().strftime("%Y-%m-%d %I-%m-%S %p").lower() + ".txt"
+            log_file_path = os.path.join(Client.LOG_DIR, log_file_name)
+            with open(log_file_path, "w") as log_file:
+                log_file.write(response.text)
             raise Exception("failed to create post: [{}]".format(response.status_code))
         post_id = int(match.group(1))
 
